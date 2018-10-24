139046fa7a90a4a10c044c459530deb89a989af2
hasan <hasanhack89@gmail.com>
2018-10-24 23:22:45 +0530

    removed temp files

--- dist/sample_app/urls.py
+++
@@ -1,6 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('<slug:name>', views.hello, name="hello"),
-]
--- dist/sample_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def hello(request, name):
-   text = f"""<h1>Welcome {name} to my app !</h1>"""
-   return HttpResponse(text)
